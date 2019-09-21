cd3a09cfd9b0e269ae798ffc4d17ee3e5ed75774
Valerie1998-lab <55493993+Valerie1998-lab@users.noreply.github.com>
2019-09-21 12:27:04 +0300

    Add files via upload

---
+++ brand.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login V10</title>
+</head>
+
+<body>
+
+<h1> HEY BRAND
+</h1>
+
+</body>
+</html>
---
+++ usage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login V10</title>
+</head>
+
+<body>
+
+<h1> HEY USAGE</h1>
+
+</body>
+</html>
